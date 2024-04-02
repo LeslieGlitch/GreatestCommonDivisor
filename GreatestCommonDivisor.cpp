9c0cfa17110b4e62bf2a3e0b27fcc29410dd0017
Leslie Glitch <77557324+LeslieGlitch@users.noreply.github.com>
2024-04-02 15:52:05 +0000

    Add GreatestCommonDenominator::gcd()

--- GreatestCommonDivisor.cpp
+++ GreatestCommonDivisor.cpp
@@ -6,3 +6,37 @@
 
 
 #include "GreatestCommonDivisor.hpp"
+#include <cmath>
+
+/// @brief Determines Greatest Common Divisor of two numbers using Stein's Algorithm
+/// @param n1 
+/// @param n2 
+/// @return GCD of n1 and n2
+int GreatestCommonDivisor::gcd(int n1, int n2) {
+    // Case: n < 0
+    n1 = abs(n1);
+    n2 = abs(n2);
+
+    // Case: n = 0
+    if (n1 == 0) return n2;
+    if (n2 == 0) return n1;
+
+    // Case: Both divisible by 2
+    if ((n1 & 1) == 0 && (n2 & 1))
+        return gcd(n1 >> 1, n2 >> 1) << 1;
+
+    // Case: n1 even, n2 odd
+    if ((n1 & 1) == 0)
+        return gcd(n1 >> 1, n2);
+    
+    // Case: n1 odd, n2 even
+    if ((n2 & 1) == 0)
+        return gcd(n1, n2 >> 1);
+
+    // Case: n1 >= n2
+    if (n1 >= n2)
+        return gcd((n1 - n2) >> 1, n2);
+    
+    // Case: n1 < n2
+    return gcd(n1, (n2 - n1) >> 1);
+}
--- GreatestCommonDivisor.hpp
+++ GreatestCommonDivisor.hpp
@@ -8,6 +8,19 @@
 #define GREATESTCOMMONDIVISOR_HPP
 
 class GreatestCommonDivisor {
+public:
+    /// @brief Determines Greatest Common Divisor of two numbers
+    /// @param n1 
+    /// @param n2 
+    /// @return GCD of n1 and n2
+    int gcd(int n1, int n2);
+
+private:
+    /// @brief Swaps two values
+    /// @param n1 
+    /// @param n2 
+    template <class T>
+    void swap(T& n1, T& n2);
 
 };
 
